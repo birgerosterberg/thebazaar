5da2f85df72005cf0f5e2c7dc2a1c05b03cf04cd
Birger <birger.oesterberg@gmail.com>
2023-11-27 15:24:33 +0100

    initial commit django

---
+++ sanda_bazaar/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for sanda_bazaar project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sanda_bazaar.settings')
+
+application = get_asgi_application()
