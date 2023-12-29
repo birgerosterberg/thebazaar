b7c6380c376b85b7a6270c455b6e0c1d05fabb08
Birger <birger.oesterberg@gmail.com>
2023-12-29 18:23:12 +0100

    add secret_key to environ

--- sanda_bazaar/settings.py
+++ sanda_bazaar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y+nnin&+ik+ie&cj%hspea5s-_=3a298vx17#%^a1e20=25l*p'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
