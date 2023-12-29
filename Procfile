547e65f13fd1ad61756519cf5273a3c8c1660612
Birger <birger.oesterberg@gmail.com>
2023-12-29 18:11:59 +0100

    deployment to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn sanda_bazaar.wsgi:application
--- sanda_bazaar/settings.py
+++ sanda_bazaar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 from decouple import config
 
     
@@ -28,7 +29,7 @@ SECRET_KEY = 'django-insecure-y+nnin&+ik+ie&cj%hspea5s-_=3a298vx17#%^a1e20=25l*p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['thebazaar-30d8729d015c.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,12 +119,17 @@ WSGI_APPLICATION = 'sanda_bazaar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
