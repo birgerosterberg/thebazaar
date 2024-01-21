ee4c23425974ae53dfbababcd5ce860355644cde
Birger <birger.oesterberg@gmail.com>
2024-01-21 22:40:58 +0100

    buggfix rating sorting desc add disclaimer image size

--- bazaar/views.py
+++ bazaar/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models.functions import Lower
-from django.db.models import Q
+from django.db.models.functions import Lower, Coalesce
+from django.db.models import Q ,Value, DecimalField
 from .models import Product, Category
 from review.models import Review
 from review.views import user_can_review_product, update_product_rating
@@ -30,7 +30,10 @@ def bazaar(request):
             elif sortkey == 'category':
                 sortkey = 'category__name'
             elif sortkey == 'rating':
-                sortkey = 'rating'
+                products = products.annotate(
+                    actual_rating=Coalesce('rating', Value(0), output_field=DecimalField())
+                )
+                sortkey = 'actual_rating'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
